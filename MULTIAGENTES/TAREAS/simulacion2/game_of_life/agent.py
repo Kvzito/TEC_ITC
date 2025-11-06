789869621ffcd0415e500d0f7ea63330f584667d
kvzito <blondednal@gmail.com>
2025-11-06 15:26:07 -0600

    Acabo simulación 2

--- MULTIAGENTES/TAREAS/simulacion2/game_of_life/agent.py
+++ MULTIAGENTES/TAREAS/simulacion2/game_of_life/agent.py
@@ -22,7 +22,7 @@ class Cell(FixedAgent):
     @property
     def neighbors(self):
         return self.cell.neighborhood.agents
-    
+
     def __init__(self, model, cell, init_state=DEAD):
         """Create a cell, in the given state, at the given x, y position."""
         super().__init__(model)
@@ -30,33 +30,50 @@ class Cell(FixedAgent):
         self.pos = cell.coordinate
         self.state = init_state
         self._next_state = None
+        self.decision = 0
+        self.top_left = 0
+        self.top_center = 0
+        self.top_right = 0
 
     def determine_state(self):
-        """Compute if the cell will be dead or alive at the next tick.  This is
-        based on the number of alive or dead neighbors.  The state is not
-        changed here, but is just computed and stored in self._nextState,
-        because our current state may still be necessary for our neighbors
-        to calculate their next state.
-        """
-        # Get the neighbors and apply the rules on whether to be alive or dead
-        # at the next tick.
 
-        live_neighbors = 0
-        for n in self.neighbors:
-            live_neighbors += n.is_alive
+        # Checamos si los vecinos están justo arriba
+        # Calculamos las coordenadas con wrapping toroidal
+        
+        width = self.model.grid.width
+        height = self.model.grid.height
 
-        # live_neighbors = sum(neighbor.is_alive for neighbor in self.neighbors)
+        top_y = (self.y + 1) % height
+        left_x = (self.x - 1) % width
+        right_x = (self.x + 1) % width
 
-        # Assume nextState is unchanged, unless changed below.
-        self._next_state = self.state
+        for neighbor in self.neighbors:
+            if neighbor.x == left_x and neighbor.y == top_y:
+                self.top_left = neighbor.is_alive
+            elif neighbor.x == self.x and neighbor.y == top_y:
+                self.top_center = neighbor.is_alive
+            elif neighbor.x == right_x and neighbor.y == top_y:
+                self.top_right = neighbor.is_alive
 
-        if self.is_alive:
-            if live_neighbors < 2 or live_neighbors > 3:
-                self._next_state = self.DEAD
-        else:
-            if live_neighbors == 3:
-                self._next_state = self.ALIVE
+        # A partir del estado de los vecinos de arriba, tomamos una decisión
+
+        if (self.top_left == 1 and self.top_center == 1 and self.top_right == 1) or \
+           (self.top_left == 0 and self.top_center == 0 and self.top_right == 0) or \
+           (self.top_left == 1 and self.top_center == 0 and self.top_right == 1) or \
+           (self.top_left == 0 and self.top_center == 1 and self.top_right == 0):
+            next_bit = 0
+        elif (self.top_left == 1 and self.top_center == 1 and self.top_right == 0) or \
+             (self.top_left == 1 and self.top_center == 0 and self.top_right == 0) or \
+             (self.top_left == 0 and self.top_center == 1 and self.top_right == 1) or \
+             (self.top_left == 0 and self.top_center == 0 and self.top_right == 1):
+            next_bit = 1
+
+        # Todas las células se actualizan, incluyendo los bordes
+        # El grid toroidal conecta automáticamente los bordes opuestos
+        self._next_state = self.ALIVE if next_bit == 1 else self.DEAD
 
     def assume_state(self):
-        """Set the state to the new computed state -- computed in step()."""
+        """Update the cell's state to the state determined in
+        the previous step.
+        """
         self.state = self._next_state
